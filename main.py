6d8a7b299426f876095b1a5f3a7dd8c775ec29b6
chengzi08 <111082713+chengzi08@users.noreply.github.com>
2025-07-08 17:59:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -266,7 +266,7 @@ def handle_message(event):
         
     elif user_message == "活動介紹":
         
-        image_url = "https://github.com/chengzi08/tsse-linebot/blob/main/ation-v2.jpg?raw=true"
+        image_url = "https://raw.github.com/chengzi08/tsse-linebot/blob/main/ation-v2.jpg"
         line_bot_api.reply_message(reply_token, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
         return
     
