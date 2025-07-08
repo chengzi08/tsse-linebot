ac09e7f6a6258df7ff976a15c77422df9563362f
chengzi08 <111082713+chengzi08@users.noreply.github.com>
2025-07-08 14:59:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-    FlexSendMessage, ImageSendMessage
+    FlexSendMessage, ImageSendMessage, ImageMessage
 )
 
 import gspread
